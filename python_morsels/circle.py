5781f347e26ca5f63525632eff14f0d7403875ec
Roger Murray <roger.murray@iis.se>
2019-09-26 22:56:35 +0200

    circle problem and all bonuses solved

--- python_morsels/circle.py
+++ python_morsels/circle.py
@@ -1,18 +1,44 @@
 #!/usr/bin/env python3
 # by rog
 
+import math
+
 
 class Circle:
-
     def __init__(self, radius=1):
-        self.radius = radius
+        self._radius = radius
+        self._diameter = radius * 2
+        self._area = math.pi * float(radius) * float(radius)
 
+    @property
     def radius(self):
-        return self.radius
+        return self._radius
 
-    def area(self):
-        return self.radius * self.radius * 3.14
+    @radius.setter
+    def radius(self, radius):
+        if radius < 0:
+            raise ValueError("Radius cannot be negative")
+        self._radius = int(radius)
+        self._diameter = self._radius * 2
+        self._area = math.pi * (self._radius) * float(self._radius)
 
+    @property
     def diameter(self):
-        return self.radius * 4
+        return self._diameter
 
+    @diameter.setter
+    def diameter(self, diameter):
+        self._diameter = diameter
+        self._radius = diameter / 2
+
+    @property
+    def area(self):
+        return self._area
+
+    def __repr__(self):
+        return "Circle(" + str(self._radius) + ")"
+
+
+if __name__ == "__main__":
+    c = Circle(5)
+    print(c.radius)
