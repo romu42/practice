ef43d19892839dfcbf97b1e9a97c3d7cb21a2615
Roger Murray <roger.murray@iis.se>
2019-10-20 19:24:27 +0200

    fstring and if/else easy bite

---
+++ bites/driving.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# by rog
+
+MIN_DRIVING_AGE = 18
+
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    if age >= 18:
+        print(f"{name} is allowed to drive")
+    else:
+        print(f"{name} is not allowed to drive")
