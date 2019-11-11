48d6ee8f94190a8096232d50e63a52263f217ee5
Roger Murray <roger.murray@iis.se>
2019-11-11 23:35:19 +0100

    bite 11 done, it was difficult, but fun

--- bites/account.py
+++ bites/account.py
@@ -17,9 +17,61 @@ class Account:
     def __len__(self):
         return len(self._transactions)
 
+
     def __add__(self, input):
-        self._transactions.append(input)
+        if isinstance(input, int):
+            self._transactions.append(input)
+        else:
+            raise ValueError
+
 
     def __sub__(self, input):
-        self._transactions.append(-input)
+        if isinstance(input, int):
+            self._transactions.append(-input)
+        else:
+            raise ValueError
+
+    def __gt__(self, other):
+        if self.balance > other.balance:
+            return True
+        else:
+            return False
+
+    def __ge__(self, other):
+        if self.balance >= other.balance:
+            return True
+        else:
+            return False
+
+
+    def __lt__(self, other):
+        if self.balance < other.balance:
+            return True
+        else:
+            return False
+
+
+    def __le__(self, other):
+        if self.balance <= other.balance:
+            return True
+        else:
+            return False
+
+
+    def __eq__(self, other):
+        if self.balance == other.balance:
+            return True
+        else:
+            return False
+
+
+    def __str__(self):
+        return f"{self.name} account - balance: {self.balance}"
+
+
+    def __getitem__(self, item):
+        return self._transactions[item]
+
+
+
 
