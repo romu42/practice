fbc97f677e95f444eeda74f5d4848ac575a36b2f
Rog <roger.murray@iis.se>
2019-12-05 21:09:43 +0100

    just checking in SKEL, not solved, taking a break for advent of code

---
+++ bites/bite161_tree.py
@@ -0,0 +1,10 @@
+# https://codechalleng.es/bites/161/
+
+import os
+
+
+def count_dirs_and_files(directory='.'):
+    """Count the amount of of directories and files in passed in "directory" arg.
+       Return a tuple of (number_of_directories, number_of_files)
+    """
+    pass
