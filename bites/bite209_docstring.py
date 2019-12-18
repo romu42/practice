2d5e4799635f61f077bb69b2b94740ee0fa5dfdb
Rog <roger.murray@iis.se>
2019-12-18 21:37:11 +0100

    100 days of python chugging along

---
+++ bites/bite209_docstring.py
@@ -0,0 +1,16 @@
+# https://codechalleng.es/bites/209/
+# https://codechaleng.es/bites/210/
+
+
+def sum_numbers(numbers):
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
+
+
+pass
