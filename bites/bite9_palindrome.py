c9e06a60882e5ff5303fe7f7d998f06951b15099
Rog <roger.murray@iis.se>
2019-12-29 22:54:37 +0100

    100 days of python

---
+++ bites/bite9_palindrome.py
@@ -0,0 +1,43 @@
+# https://codechalleng.es/bites/9/
+
+"""A palindrome is a word, phrase, number, or other sequence of characters
+which reads the same backward as forward"""
+import os
+import urllib.request
+import re
+
+DICTIONARY = os.path.join('/tmp', 'dictionary_m_words.txt')
+urllib.request.urlretrieve('http://bit.ly/2Cbj6zn', DICTIONARY)
+
+
+def load_dictionary():
+    """Load dictionary (sample) and return as generator (done)"""
+    with open(DICTIONARY) as f:
+        return (word.lower().strip() for word in f.readlines())
+
+
+def is_palindrome(word):
+    """Return if word is palindrome, 'madam' would be one.
+       Case insensitive, so Madam is valid too.
+       It should work for phrases too so strip all but alphanumeric chars.
+       So "No 'x' in 'Nixon'" should pass (see tests for more)"""
+    word = re.sub(r'[\W_]+', '', word.lower())
+    reverse_word = word[::-1]
+    if word == reverse_word:
+        return True
+    else:
+        return False
+
+
+def get_longest_palindrome(words=None):
+    """Given a list of words return the longest palindrome
+       If called without argument use the load_dictionary helper
+       to populate the words list"""
+    words_list = []
+    if words is None:
+        words = load_dictionary()
+
+    for word in words:
+        if is_palindrome(word):
+            words_list.append(word)
+    return max(words_list, key=len)
