6943b03458c876b149c6d402ff4d561bf39515a5
Rog <roger.murray@iis.se>
2020-01-05 11:59:23 +0100

    100 days of python

---
+++ bites/bite17_friends.py
@@ -0,0 +1,11 @@
+# https://codechalleng.es/bites/17/
+
+from itertools import permutations, combinations
+
+
+def friends_teams(friends, team_size=2, order_does_matter=False):
+    if order_does_matter:
+        return permutations(friends, team_size)
+    else:
+        return combinations(friends, team_size)
+
