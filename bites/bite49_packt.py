af642eee1905d6bf8814e9f19436a3e0559cb4f3
Rog <roger.murray@iis.se>
2020-01-21 22:55:16 +0100

    100 days of python

--- bites/bite49_packt.py
+++ bites/bite49_packt.py
@@ -7,6 +7,7 @@ from collections import namedtuple
 from bs4 import BeautifulSoup as Soup
 import requests
 import pprint
+import re
 
 pp = pprint.PrettyPrinter()
 
@@ -20,21 +21,21 @@ Book = namedtuple('Book', 'title description image link')
 def get_book():
     """make a Soup object, parse the relevant html sections, and return a Book namedtuple"""
     soup = Soup(CONTENT, 'lxml')
-    #mydivs = soup.find_all('div', id='deal-of-the-day')
-    #print(dir(mydivs))
-    #description = mydivs[0].contents[1].text.replace('\t', '')
-    #print((description.split('\n')))
-    mydivs = soup.find_all('div', class_='dotd-main-book-summary float-left')
-
-    for div in mydivs:
-        #print(div)
-        print('bobobobobobobobbo')
-        print(div.text.strip())
     # works for title
-    #title = soup.find('div', class_='dotd-title')
+    divs = soup.find('div', class_='dotd-title')
     #print(title.text.strip())
-    #img = soup.find_all('div', class_='bookimage imagecache imagecache-dotd_main_image')
-    #print(img.text.strip())
+    title = (divs.text.strip())
+    next = divs.next_sibling
+    next = next.next_sibling
+    next = next.next_sibling
+    #print(next.next_sibling.text.strip())
+    description = (next.next_sibling.text.strip())
+    divs = soup.find('div', class_='dotd-main-book-image float-left')
+    #print((image.contents[1].contents[1].contents[0].attrs['src']))
+    image = ((divs.contents[1].contents[1].contents[0].attrs['src']))
+    #print((image.contents[1].attrs['href']))
+    link = ((divs.contents[1].attrs['href']))
+    return Book(title, description, image, link)
 
 if __name__ == '__main__':
     get_book()
